6154a8711e51087c76dfcdf743dc8fa8e0f98164
Rizky Hermawan <rhermawan56@gmail.com>
2022-08-14 01:59:41 +0700

    hero: updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,45 @@
 $('#start .start-close').click(function () {
     $('#start').hide()
+    $(location).attr('href', '#home')
+})
+
+let heroIndex
+let target
+let imgTarget
+let contentTarget
+// $('.hero-card img').click(function (params) {
+//     console.log($(this).index())
+// })
+
+$(".f-index").hover(function () {
+    heroIndex = $(".f-index").index(this)
+    
+    const content = document.getElementsByClassName('content')[heroIndex]
+    const img = document.getElementsByClassName('hero-img')[heroIndex]
+    target = document.getElementsByClassName('hero-card')[heroIndex]
+
+    imgTarget = $(img).attr('src')
+    contentTarget = $(content).html()
+    $(target).addClass('hover-class')
+})
+
+$(".f-index").mouseout(function () {
+    heroIndex = $(".f-index").index(this)
+    console.log(heroIndex)
+    target = document.getElementsByClassName('hero-card')[heroIndex]
+    $(target).removeClass('hover-class')
+})
+
+$(".f-index").click(function () {
+    $('#show').show()
+    $('#content').html(contentTarget)
+    $('#image').attr('src', imgTarget)
+})
+
+$('#close-show').click(function () {
+    $('#show').hide()
 })
 
 $(function () {
-    console.log("ok")
+
 })
